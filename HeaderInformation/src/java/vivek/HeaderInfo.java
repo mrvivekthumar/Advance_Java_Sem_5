f56cb00e608b0a48eebfaacac7aa84f9f015313b
mrvivekthumar <mrvivekthumar@gmail.com>
2024-09-09 12:04:44 +0530

    Added Login using http Session

--- HeaderInformation/src/java/vivek/HeaderInfo.java
+++ HeaderInformation/src/java/vivek/HeaderInfo.java
@@ -10,12 +10,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Enumeration;
 
-/**
- *
- * @author thuma
- */
 public class HeaderInfo extends HttpServlet {
 
     @Override
@@ -43,5 +41,53 @@ public class HeaderInfo extends HttpServlet {
         }
         out.println("</TABLE>");
         out.println("</BODY></HTML>");
+
+//        Paper Question for Question no 3 or (b)
+//        String clientBrowser = request.getHeader("User-Agent");
+//        String clientIP = request.getRemoteAddr();
+//        String methodUsed = request.getMethod();
+//        int clientPort = request.getRemotePort();
+//        String queryString = request.getQueryString();
+//
+//        // Set response content type
+//        response.setContentType("text/html");
+//
+//        // Display client information
+//        response.getWriter().println("<h2>Client Information</h2>");
+//        response.getWriter().println("<p><b>Client Browser:</b> " + clientBrowser + "</p>");
+//        response.getWriter().println("<p><b>Client IP Address:</b> " + clientIP + "</p>");
+//        response.getWriter().println("<p><b>Method Used for Form Submission:</b> " + methodUsed + "</p>");
+//        response.getWriter().println("<p><b>Client Port Number:</b> " + clientPort + "</p>");
+//        response.getWriter().println("<p><b>Query String Name and Values:</b> " + queryString + "</p>");
+
+//    Paper Question for Question no 2  (c)    
+        String clientIP = request.getRemoteAddr();
+        String clientBrowser = request.getHeader("User-Agent");
+        String methodUsed = request.getMethod();
+
+        // Try to get hostname from IP address
+        String clientHostname = "unknown";
+        try {
+            clientHostname = InetAddress.getByName(clientIP).getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        // Log client details
+        System.out.println("Client Hostname: " + clientHostname);
+        System.out.println("Client IP Address: " + clientIP);
+        System.out.println("Client Browser: " + clientBrowser);
+        System.out.println("HTTP Method: " + methodUsed);
+
+        // Get search parameter
+        String searchQuery = request.getParameter("search");
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Display search result (this is a placeholder, no actual search is performed)
+        response.getWriter().println("<h2>Search Results for: " + searchQuery + "</h2>");
+        response.getWriter().println("<p>Client details have been logged.</p>");
+    
     }
 }
---
+++ Sessional2/src/java/vivek/Login.java
@@ -0,0 +1,70 @@
+package vivek;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class Login extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        HttpSession session = request.getSession();
+
+        session.setAttribute("username", username);
+        session.setAttribute("password", password);
+
+        response.getWriter().println("<form action='Dashboard' method='post'>");
+        response.getWriter().println("<input type=\"text\" name=\"username\">");
+        response.getWriter().println("<input type=\"text\" name=\"password\">");
+        response.getWriter().println("<input type=\"submit\" value=\"Submit\">");
+        response.getWriter().println("</form>");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
